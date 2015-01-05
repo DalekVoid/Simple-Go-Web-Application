719c269c439e1f708b6876328338d8897d97075c
Dalek Void <dalek@voidamplifier.com>
2015-01-05 18:52:43 +0800

    before handling possible error in loadPage(string)

--- wiki.go
+++ wiki.go
@@ -5,3 +5,20 @@ import (
   "io/ioutil"
 )
 
+type Page struct {
+  Title string
+  Body []byte
+}
+
+func (p *Page) save() error {
+  filename := p.Title + ".txt"
+  return ioutil.WriteFile(filename, p.Body, 0600) // 0600 for read-write permission.
+}
+
+func loadPage(title string) *Page {
+  filename := title + ".txt"
+  body, _  := ioutil.ReadFile(filename) // wildcard character(is it how it is called in go?(oh it is called the blank identifier)) throwaway the unwanted error value.
+  return &Page{Title: title, Body: body}
+}
+
+
