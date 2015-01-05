97e8d958b95d3b0df27fa207cd0afa78c1414c51
Dalek Void <dalek@voidamplifier.com>
2015-01-05 20:17:21 +0800

    added save handler

--- wiki.go
+++ wiki.go
@@ -34,7 +34,11 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
   title := r.URL.Path[len("/view/"):] // len to slice the url and slice leading "/view/" in the request
-  p, _  := loadPage(title)
+  p, err  := loadPage(title)
+  if err != nil {
+    http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+    return
+  }
   renderTemplate(w, "view", p)
 }
 
@@ -48,8 +52,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-
+  title := r.URL.Path[len("/save/"):]
+  body := r.FormValue("body")
+  p := &Page{Title: title, Body: []byte(body)}
+  p.save()
+  http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
+
 func main() {
   http.HandleFunc("/view/", viewHandler)
   http.HandleFunc("/edit/", editHandler)
