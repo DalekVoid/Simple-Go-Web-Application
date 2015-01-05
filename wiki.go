3a7d0c6022beb5141f1dd8e036ebb9f146614fd2
Dalek Void <dalek@voidamplifier.com>
2015-01-05 18:57:03 +0800

    can handle error in loadPage(string) now. Ready to go.

--- wiki.go
+++ wiki.go
@@ -15,10 +15,18 @@ func (p *Page) save() error {
   return ioutil.WriteFile(filename, p.Body, 0600) // 0600 for read-write permission.
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
   filename := title + ".txt"
-  body, _  := ioutil.ReadFile(filename) // wildcard character(is it how it is called in go?(oh it is called the blank identifier)) throwaway the unwanted error value.
-  return &Page{Title: title, Body: body}
+  body, err := ioutil.ReadFile(filename)
+  if err != nil {
+    return nil, err
+  }
+  return &Page{Title: title, Body: body}, nil
 }
 
-
+func main() {
+  p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+  p1.save()
+  p2, _ := loadPage("TestPage")
+  fmt.Println(string(p2.Body))
+}
